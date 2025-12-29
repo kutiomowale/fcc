f2320cea8743a6b0745ad0b4a59c45265a5fbce8
RANSOME-KUTI OMOWALE EMMANUEL <omowaleransomekuti@gmail.com>
2025-12-29 01:03:12 +0100

    Pycodesyle clean up

--- implement_the_tower_of_hanoi_algorithm.py
+++ implement_the_tower_of_hanoi_algorithm.py
@@ -10,9 +10,7 @@ def TowerOfHanoi(n, from_rod, to_rod, aux_rod):
     TowerOfHanoi(n-1, aux_rod, to_rod, from_rod)
 
 
-
 # Contributed By Harshit Agrawal
-
 
 
 def main():
--- rpg.py
+++ rpg.py
@@ -1,5 +1,8 @@
+#!/usr/bin/env python3
 full_dot = '●'
 empty_dot = '○'
+
+
 def create_character(name, strength, intelligence, charisma):
     if not isinstance(name, str):
         return "The character name should be a string"
@@ -7,7 +10,10 @@ def create_character(name, strength, intelligence, charisma):
         return "The character name is too long"
     if any(char.isspace() for char in name):
         return "The character name should not contain spaces"
-    if any(not isinstance(stat, int) for stat in (strength, intelligence, charisma)):
+    if any(
+         not isinstance(stat, int)
+         for stat in (strength, intelligence, charisma)
+    ):
         return "All stats should be integers"
     if any(stat < 1 for stat in (strength, intelligence, charisma)):
         return "All stats should be no less than 1"
@@ -17,7 +23,15 @@ def create_character(name, strength, intelligence, charisma):
         return "The character should start with 7 points"
 
     line1 = name + '\n'
-    line2 = "STR " + (full_dot * strength) + (empty_dot * (10 - strength)) + '\n'
-    line3 = "INT " + (full_dot * intelligence) + (empty_dot * (10 - intelligence)) + '\n'
+    line2 = (
+        "STR "
+        f"(full_dot * strength)"
+        f"(empty_dot * (10 - strength))\n"
+    )
+    line3 = (
+        "INT "
+        f"(full_dot * intelligence) + (empty_dot * (10 - intelligence))"
+        f"\n"
+        )
     line4 = "CHA " + (full_dot * charisma) + (empty_dot * (10 - charisma))
     return line1 + line2 + line3 + line4
